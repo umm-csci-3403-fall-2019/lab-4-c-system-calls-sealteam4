09353c56d73e5162d80ef703c4ce2a74abd8e10a
unwarysage <shawx538@morris.umn.edu>
2019-10-08 11:39:50 -0500

    Switch to familiar implementation of disemvowel

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -4,43 +4,51 @@
 #include <stdio.h>
 
 
+//Declare a list of all vowels
+const char* all_vowels = "aeiouAEIOU";
+
+
+int is_vowel(char inp_char){
+
+  int vowel_status = 0;
+  int j=0;
+  for(j=0;all_vowels[j] != '\0';j++){
+    if (all_vowels[j] == inp_char){
+      vowel_status = 1;
+      break;
+    }
+  }
+  return vowel_status;
+}
+
 char *disemvowel(char *str) {
-    //if we are given a null pointer, operate on an empty string
-	if (str == 0x0){
+  if(str==0x0){
+   str = "";
+  }
+  char *disemvoweled_string;
+  int i;
+  int vowel_count = 0;
+  int str_length = strlen(str);
+    //in this loop, get a count of consonants
+  for(i=0; str[i] != '\0';i++){
+    if (is_vowel(str[i]) == 1){
+      vowel_count++;
+    }
+  }
 
-    }
+  //declare a newstring of consonant count +1 size
+  int pos_in_disemvoweled=0;
 
-    int i;
-    int numVowel = 0;
-    int length = strlen(str);
-    for (i=0; i<length; i++){
-        switch(tolower(str[i])){
-	    case 'a':
-            case 'e':
-            case 'i':
-	    case 'o':
-            case 'u':
-                numVowel = numVowel + 1;
-		break;
-	}
+  disemvoweled_string = (char*) calloc(str_length - vowel_count+1, sizeof(char));
+
+  for(i=0; str[i] != '\0';i++){
+    if (is_vowel(str[i]) == 0){
+      disemvoweled_string[pos_in_disemvoweled]=str[i];
+      pos_in_disemvoweled++;
     }
-    char *newstr = (char*) calloc(length - numVowel + 1, sizeof(char));
-    int j=0;
-    for (i=0; i<length; i++){
-        switch(tolower(str[i])){
-            case 'a':
-            case 'e':
-            case 'i':
-            case 'o':
-            case 'u':
-                break;
-            default:
-		newstr[j] = str[i];
-		j = j + 1;
-		break;
-	}
-    }
-  return newstr;
+  }
+
+  return (char*) disemvoweled_string;
 }
 
 int main(int argc, char *argv[]) {
@@ -56,13 +64,11 @@ int main(int argc, char *argv[]) {
   }
 
   char* str;
-  char* input;
   int notDone = 1;
   char* buffer = calloc(1000, sizeof(char));
   int str_length = 0;
   while(notDone) {
-     input = fgets(buffer,1000,stdin);
-     str = disemvowel(input);
+     str = disemvowel(fgets(buffer,1000,stdin));
      str_length = strlen(str);
      fwrite((void*)str, sizeof(char), str_length, stdout);
      fflush(stdout);

5376bdaf480c1365fe6261f059cbf92c3f38d3a5
unwarysage <shawx538@morris.umn.edu>
2019-10-10 11:33:09 -0500

    Add base implementation using the provided format
    fails bats fore file input output
    fails valgirnd errrors
    passes valgrind leaks

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -1,15 +1,18 @@
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+#define BUF_SIZE 1024
 
 //Declare a list of all vowels
 const char* all_vowels = "aeiouAEIOU";
 
 
-int is_vowel(char inp_char){
-
+bool is_vowel(char inp_char) {
+    /*
+     * Returns true if c is a vowel (upper or lower case), and
+     * false otherwise.
+     */
   int vowel_status = 0;
   int j=0;
   for(j=0;all_vowels[j] != '\0';j++){
@@ -21,68 +24,75 @@ int is_vowel(char inp_char){
   return vowel_status;
 }
 
-char *disemvowel(char *str) {
-  if(str==0x0){
-   str = "";
-  }
-  char *disemvoweled_string;
-  int i;
-  int vowel_count = 0;
-  int str_length = strlen(str);
-    //in this loop, get a count of consonants
-  for(i=0; str[i] != '\0';i++){
-    if (is_vowel(str[i]) == 1){
-      vowel_count++;
-    }
-  }
 
-  //declare a newstring of consonant count +1 size
-  int pos_in_disemvoweled=0;
+int copy_non_vowels(int num_chars, char* in_buf, char* out_buf) {
+    /*
+     * Copy all the non-vowels from in_buf to out_buf.
+     * num_chars indicates how many characters are in in_buf,
+     * and this function should return the number of non-vowels that
+     * that were copied over.
+     */
+	int consonant_count = 0;
+	for (int i = 0; i < num_chars; i++){
+		if (!is_vowel(in_buf[i])){
+			out_buf[consonant_count] = in_buf[i];
+			consonant_count++;
+		}
+	}
+	return consonant_count;
+}
 
-  disemvoweled_string = (char*) calloc(str_length - vowel_count+1, sizeof(char));
+void disemvowel(FILE* inputFile, FILE* outputFile) {
+    /*
+     * Copy all the non-vowels from inputFile to outputFile.
+     * Create input and output buffers, and use fread() to repeatedly read
+     * in a buffer of data, copy the non-vowels to the output buffer, and
+     * use fwrite to write that out.
+     */
+	//New buffers
+ 	char* in_buffer = calloc(BUF_SIZE,sizeof(char));
+	char* out_buffer = calloc(BUF_SIZE,sizeof(char));
+	//Boolean for our loop to exit
+	bool cont = true;
+	while(cont){
+		// an int to count in how many items we read in
+		int read_count = 0;
+		// do the actual reading now
+		read_count=fread(in_buffer,sizeof(char),BUF_SIZE, inputFile);
+		int consonant_count = 0;
+		consonant_count=copy_non_vowels(read_count,in_buffer,out_buffer);
+		//we now have the disemvoweled string in out_buffer
+		fwrite(out_buffer,sizeof(char),consonant_count,outputFile);
+		//We have no more things to do with the buffers, so free their contents
+		free(in_buffer);
+		free(out_buffer);
+		//Exit the loop if we reahc the end of the input. (or encounter an error)
+		if(read_count != BUF_SIZE){
+			cont=false;
+		}
+	}
 
-  for(i=0; str[i] != '\0';i++){
-    if (is_vowel(str[i]) == 0){
-      disemvoweled_string[pos_in_disemvoweled]=str[i];
-      pos_in_disemvoweled++;
-    }
-  }
-
-  return (char*) disemvoweled_string;
 }
 
 int main(int argc, char *argv[]) {
-  if(argc >= 2) {
-    freopen(argv[1], "r", stdin);
-    if(!stdin) {
-      exit(EXIT_FAILURE);
-    }
-  }
-  else if (argc == 3) {
-    printf("Output to:");
-    freopen(argv[2], "w", stdout);
-  }
+    	// You should set these to `stdin` and `stdout` by default
+    	// and then set them to user specified files when the user
+    	// provides files names as command line arguments.
+    	FILE *inputFile = stdin;
+    	FILE *outputFile = stdout;
+	//if we have more than two args, then we at least have input file
+	if(argc >= 2){
+		inputFile = fopen(argv[1], "r");
+	}
+	//if we have three args, then we have output file
+	if (argc ==3){
+		outputFile = fopen(argv[2],"w");
+	}
+    	// Code that processes the command line arguments
+    	// and sets up inputFile and outputFile.
 
-  char* str;
-  int notDone = 1;
-  char* buffer = calloc(1000, sizeof(char));
-  int str_length = 0;
-  while(notDone) {
-     str = disemvowel(fgets(buffer,1000,stdin));
-     str_length = strlen(str);
-     fwrite((void*)str, sizeof(char), str_length, stdout);
-     fflush(stdout);
-     if(feof(stdin)) {
-       notDone = 0;
-     }
-     if(ferror(stdin)){
-     	notDone = 0;
-     }
-     free(input);
-     free(str);
-   }
-  
-
-  fclose(stdout);
-  fclose(stdin);
+    	disemvowel(inputFile, outputFile);
+	fclose(outputFile);
+	fclose(inputFile);
+    	return 0;
 }

caa27e044a408ce4e64e11fea8e8ddbd696392f2
UnwarySage <shawx538@morris.umn.edu>
2019-10-16 18:17:23 -0500

    Fix file IO weirdness

--- file_disemvowel/file_disemvowel.c
+++ file_disemvowel/file_disemvowel.c
@@ -52,15 +52,15 @@ char *disemvowel(char *str) {
 }
 
 int main(int argc, char *argv[]) {
-  if(argc >= 2) {
-    freopen(argv[1], "r", stdin);
-    if(!stdin) {
-      exit(EXIT_FAILURE);
-    }
+  FILE *input_addr = stdin;
+  FILE *output_addr = stdout;
+
+  if (argc == 2){
+    input_addr = fopen(argv[1],"r");
   }
-  else if (argc == 3) {
-    printf("Output to:");
-    freopen(argv[2], "w", stdout);
+  if (argc == 3){
+    input_addr = fopen(argv[1],"r");
+    output_addr = fopen(argv[2],"w");
   }
 
   char* str;
@@ -68,21 +68,19 @@ int main(int argc, char *argv[]) {
   char* buffer = calloc(1000, sizeof(char));
   int str_length = 0;
   while(notDone) {
-     str = disemvowel(fgets(buffer,1000,stdin));
+     str = disemvowel(fgets(buffer,1000,input_addr));
      str_length = strlen(str);
-     fwrite((void*)str, sizeof(char), str_length, stdout);
-     fflush(stdout);
-     if(feof(stdin)) {
+     fwrite((void*)str, sizeof(char), str_length, output_addr);
+     fflush(output_addr);
+     if(feof(input_addr)) {
        notDone = 0;
      }
-     if(ferror(stdin)){
+     if(ferror(input_addr)){
      	notDone = 0;
      }
      free(str);
    }
    free(buffer);
-
-
   fclose(stdout);
   fclose(stdin);
 }
